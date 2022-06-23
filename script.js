c1e73a0376aedd1a8671d98e1372be4b4c792c4e
Erwin <erwin.stein@gmail.com>
2022-06-23 00:14:28 -0300

    22/06/22 -- 23:50

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
+// SELETORES E VARIAVEIS
 
 
-// ESCONDE/MOSTRA NAVBAR SCROLL
+
+// ESCONDE/MOSTRA NAVBAR/MODALS SCROLL
 window.onscroll = function() {
-var navbar = document.querySelector(".navbar");
-var navBg = getComputedStyle(document.documentElement).getPropertyValue('--cor-bg-10'); 
+  var navbar = document.querySelector(".navbar");
+  var navBg = getComputedStyle(document.documentElement).getPropertyValue('--cor-bg-10'); 
+  var shadow = getComputedStyle(document.documentElement).getPropertyValue('--box-shadow'); 
+  var modalLinks = document.querySelector('.modal-links-container');
 
   if (window.pageYOffset > 0) {
     navbar.style.backgroundColor = "transparent";
     navbar.style.boxShadow = "none";
   } else {
     navbar.style.backgroundColor = `rgba(${navBg},1)`;
+    navbar.style.boxShadow = `${shadow}`;
   }
 
 } 
+
+// ABRE MODAL LINKS
+function abreModalLink() {
+  window.scrollTo(0, 0);
+  var modalLinks = document.querySelector('.modal-links-container');
+  var pos = `${window.scrollY}`;
+  modalLinks.style.top = `calc(${pos}px + 20px)`;  
+  modalLinks.classList.toggle('hidden');
+}
+// FECHA MODAL LINKS
+function fechaModalLink() {
+  var modalLinks = document.querySelector('.modal-links-container');
+  if (!modalLinks.classList.contains('hidden')) {
+    modalLinks.classList.toggle('hidden');
+  }
+}
