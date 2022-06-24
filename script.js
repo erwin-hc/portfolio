c3c077a9a258d62a6d47715a1d29e862752c5b19
erwin-hc <erwin.stein@gmail.com>
2022-06-24 16:26:15 -0300

    24/06/22 - 16:26

--- script.js
+++ script.js
@@ -1,64 +1,102 @@
 // SELETORES E VARIAVEIS
+var body = document.querySelector('body');
 var modalConfig = document.querySelector('.modal-config-container');
 var modalLinks = document.querySelector('.modal-links-container');
 var seletorCores = document.querySelector('.cores-container');
 var navbar = document.querySelector(".navbar");
 var navBg = getComputedStyle(document.documentElement).getPropertyValue('--cor-bg-med'); 
 var shadow = getComputedStyle(document.documentElement).getPropertyValue('--box-shadow'); 
+var iconeLogo = document.querySelector('.logo-svg');
+var iconeConfig = document.querySelector('.conf-svg');
+var container = document.querySelector('.flex-container');
+var ulLinks = document.querySelector('.ul-container');
 
 /*-------------------------------------------------------------------------------------------------*/
 // ESCONDE/MOSTRA NAVBAR/MODALS SCROLL
-window.onscroll = function() {
+// window.onscroll = function() {
 
-  if (window.pageYOffset > 0) {
-    navbar.style.backgroundColor = "transparent";
-    navbar.style.boxShadow = "none";
-  } else {
-    navbar.style.backgroundColor = `rgba(${navBg},1)`;
-    navbar.style.boxShadow = `${shadow}`;
-  }
-
-  if (window.pageYOffset > 500) {
-    modalLinks.classList.add('hidden');
-    modalConfig.classList.add('hidden');
-  }
-} 
+//   if (window.pageYOffset > 0) {
+//     navbar.style.backgroundColor = "transparent";
+//     navbar.style.boxShadow = "none";
+//   } else {
+//     navbar.style.backgroundColor = `rgba(${navBg},1)`;
+//     navbar.style.boxShadow = `${shadow}`;
+//   }
+// } 
 /*-------------------------------------------------------------------------------------------------*/
-// ABRE MODAL LINKS
+// TRAVA SCROLLBAR
+function travaScrollBars() {
+    document.documentElement.style.overflow = 'hidden';
+}
+// DESTRAVA SCROLLBAR
+function destravaScrollBars() {
+    document.documentElement.style.overflow = 'auto';
+}
+/*-------------------------------------------------------------------------------------------------*/
+// ABRE E FECHA MODAL LINKS
+iconeLogo.addEventListener("click", function () {
+    if (modalLinks.classList.contains('hidden')) {
+      abreModalLink();
+    } else {
+      fechaModalLink();
+    }
+})
+// --------------------------------------------------
 function abreModalLink() {
-  window.scrollTo(0, 0);
+  travaScrollBars();
+  var top = window.scrollY + 90;
+  modalLinks.style.top = `${top}px`;
+  modalLinks.classList.toggle('hidden');
 
   if (!modalConfig.classList.contains('hidden')) {
     modalConfig.classList.toggle('hidden');
-  }
-  
-  modalLinks.classList.toggle('hidden');
+  }  
 }
-// FECHA MODAL LINKS
-function fechaModalLink() {
-  
+// --------------------------------------------------
+function fechaModalLink() {  
+  destravaScrollBars();
+  // body.style.overflow = 'scroll';
   if (!modalLinks.classList.contains('hidden')) {
     modalLinks.classList.toggle('hidden');
   }
 }
 /*-------------------------------------------------------------------------------------------------*/
-// ABRE MODAL CONFIG
+// ABRE E FECHA MODAL CONFIG
+iconeConfig.addEventListener("click", function () {
+    if (modalConfig.classList.contains('hidden')) {
+      abreModalConfig();
+    } else {
+      fechaModalConfig();
+    }
+})
+// --------------------------------------------------
 function abreModalConfig() {
-  window.scrollTo(0, 0);
+  travaScrollBars();
+  var top = window.scrollY + 90;
+  modalConfig.style.top = `${top}px`;
+  modalConfig.classList.toggle('hidden');
 
   if (!modalLinks.classList.contains('hidden')) {
   modalLinks.classList.toggle('hidden');
-  }
- 
-  modalConfig.classList.toggle('hidden');
+  }   
 }
-// FECHA MODAL CONFIG
+// --------------------------------------------------
 function fechaModalConfig() {
-
+   destravaScrollBars(); 
   if (!modalConfig.classList.contains('hidden')) {
     modalConfig.classList.toggle('hidden');
   }
 }
+// FECHA MODAL AO CLICAR NA TELA
+container.addEventListener("click", function () {
+  fechaModalLink();
+  fechaModalConfig();
+})
+// FECHA MODAL AO CLICAR NO LINK 
+ulLinks.addEventListener('click', function (el) {
+  fechaModalLink();
+  fechaModalConfig();
+})
 /*-------------------------------------------------------------------------------------------------*/
 // SELECIONA COR TEMA
 seletorCores.addEventListener('click', function (el) {
@@ -67,12 +105,8 @@ seletorCores.addEventListener('click', function (el) {
   getComputedStyle(document.documentElement).getPropertyValue(`--svg-${i}`);
   document.documentElement.style.setProperty('--cor-tema', `var(--cor-${i})`);
   document.documentElement.style.setProperty('--cor-icones-svg', `var(--svg-${i})`);
-  modalConfig.classList.toggle('hidden');
+  fechaModalConfig();
 });
-
-
-
-
 
 
 
@@ -82,14 +116,14 @@ seletorCores.addEventListener('click', function (el) {
 // SCROLL REVAL
 /*-------------------------------------------------------------------------------------------------*/
 
-const sr = ScrollReveal ({
-    origin: 'top',
-    distance: '40px',
-    duration: 1000,
-    reset: true
-});
+// const sr = ScrollReveal ({
+//     origin: 'top',
+//     distance: '20px',
+//     duration: 1000,
+//     reset: true
+// });
 
-sr.reveal(`.section-title`, {
-    interval: 100
-})
+// sr.reveal(`.section-title`, {
+//     interval: 100
+// })
 /*-------------------------------------------------------------------------------------------------*/
